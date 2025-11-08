9f3bb6d48c173cf1a18398a9f941a08ee9ea5273
Vinicius <vinipg1@hotmail.com>
2025-11-07 23:45:33 -0300

    feat: criando rota para doc via swagger

--- mogo/mogo/urls.py
+++ mogo/mogo/urls.py
@@ -1,26 +1,17 @@
-"""
-URL configuration for mogo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),
-    path('social/', include('social.urls')),
-    path('navigation/', include('navigation.urls')),
+    path("admin/", admin.site.urls),
+    path("usuarios/", include("usuarios.urls")),
+    path("social/", include("social.urls")),
+    path("navigation/", include("navigation.urls")),
     ## path('analysis/', include('analysis.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
